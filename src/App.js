ea427869201b568763adc9f2a0f402168de77a43
dfrancese <davefrancese@gmail.com>
2018-06-26 19:29:29 -0600

    render dummy data messages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MessageList from "./components/MessageList";
+import SendMessageForm from "./components/SendMessageForm";
+import RoomList from "./components/RoomList";
+import NewRoomForm from "./components/NewRoomForm";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <MessageList />
       </div>
     );
   }
